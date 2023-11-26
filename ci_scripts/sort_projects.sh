b52f755542cec4411664aadced43a97dd525a7ad
BB9z <bb9z@me.com>
2023-11-26 23:01:23 +0800

    fix script

--- ci_scripts/sort_projects.sh
+++ ci_scripts/sort_projects.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # 搜寻目录下的 Xcode 项目文件，对项目文件执行排序整理操作
-# Copyright © 2018, 2022 BB9z.
+# Copyright © 2018, 2022-2023 BB9z.
 # https://github.com/BB9z/iOS-Project-Template
 
 set -euo pipefail
@@ -10,7 +10,7 @@ readonly ScriptPath=$(dirname $0)
 for file in $(find . -name "*.xcodeproj" -maxdepth 2); do
     if [[ "$file" == *Pods.xcodeproj ]]; then
         # echo "跳过 pod"
-        return
+        continue
     fi
     echo "整理: $file"
     perl -w "$ScriptPath/sort-Xcode-project-file.pl" "$file"
